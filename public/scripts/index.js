826cd94115cfc09e06d9261a8bbe6a6e81eb3faa
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-06-02 19:48:38 -0300

    Cover now animate out
    - The cover animate out once the cover animation completes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,6 +2,7 @@ import anime from "../../node_modules/animejs/lib/anime.es.js";
 const cover = document.querySelector("#cover");
 const namebox = cover.querySelector("#name-box");
 const surname = namebox.querySelector("#surname");
+const firstname = namebox.querySelector("#firstname");
 
 const breakWord = (element) => {
     element.innerHTML = element.innerText.replace(
@@ -11,6 +12,9 @@ const breakWord = (element) => {
 };
 
 breakWord(surname);
+
+const coverTL = anime.timeline();
+const coverRevealTimeMS = 1300;
 
 surname.querySelectorAll(".char").forEach((el, index) => {
     const positionArrX = ["-100%", "0%", "100%", "-100%", "0%", "100%"];
@@ -22,12 +26,29 @@ surname.querySelectorAll(".char").forEach((el, index) => {
     });
 });
 
-anime({
-    targets: "#surname .char",
-    translateX: 0,
-    translateY: 0,
-    scale: 1,
-    duration: 350,
-    easing: "easeOutCirc",
-    delay: anime.stagger(150, { grid: [3, 2], from: "center" }),
-});
+coverTL
+    .add({
+        targets: "#surname .char",
+        translateX: 0,
+        translateY: 0,
+        scale: 1,
+        duration: 350,
+        easing: "easeOutCirc",
+        delay: anime.stagger(150, { grid: [3, 2], from: "center" }),
+    })
+    .add({
+        targets: surname,
+        scale: 1.75,
+        duration: coverRevealTimeMS,
+        easing: "easeInOutCubic",
+        delay: 150,
+    })
+    .add(
+        {
+            targets: cover,
+            height: 0,
+            duration: coverRevealTimeMS,
+            easing: "easeInOutCubic",
+        },
+        `-=${coverRevealTimeMS}`
+    );
