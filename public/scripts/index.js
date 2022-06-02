dae84c6ad779ce7297863850b1ec1cf0aa457d4d
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-06-02 19:03:21 -0300

    Added cover surname animation
    - The letter in the surname transition into view

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
+import anime from "../../node_modules/animejs/lib/anime.es.js";
 const cover = document.querySelector("#cover");
 const namebox = cover.querySelector("#name-box");
 const surname = namebox.querySelector("#surname");
@@ -10,3 +11,23 @@ const breakWord = (element) => {
 };
 
 breakWord(surname);
+
+surname.querySelectorAll(".char").forEach((el, index) => {
+    const positionArrX = ["-100%", "0%", "100%", "-100%", "0%", "100%"];
+    const positionArrY = ["-100%", "-100%", "-100%", "100%", "100%", "100%"];
+    anime.set(el, {
+        translateX: positionArrX[index],
+        translateY: positionArrY[index],
+        scale: 0,
+    });
+});
+
+anime({
+    targets: "#surname .char",
+    translateX: 0,
+    translateY: 0,
+    scale: 1,
+    duration: 350,
+    easing: "easeOutCirc",
+    delay: anime.stagger(150, { grid: [3, 2], from: "center" }),
+});
