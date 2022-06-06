eb1b94e559f6bfd3a135454cb375bd877f501758
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-06-06 19:33:10 -0300

    Updated logo animation

--- src/scripts/homePage.js
+++ src/scripts/homePage.js
@@ -10,30 +10,41 @@ breakWord(surname);
 
 anime.set(surname.querySelectorAll(".char p"), {
     translateX: "110%",
+    rotateZ: "-70deg",
 });
 
 logo.addEventListener("mouseover", () => {
     anime({
         targets: surname.querySelectorAll(".char p"),
         translateX: 0,
+        rotateZ: 0,
         easing: "easeInOutQuart",
     });
-    anime({
-        targets: firstname.querySelectorAll(".char p"),
-        translateX: "-110%",
-        easing: "easeInOutQuart",
-    });
+    anime(
+        {
+            targets: firstname.querySelectorAll(".char p"),
+            translateX: "-110%",
+            rotateZ: "-70deg",
+            easing: "easeInOutQuart",
+        },
+        "-=1000"
+    );
 });
 
 logo.addEventListener("mouseleave", () => {
     anime({
         targets: surname.querySelectorAll(".char p"),
         translateX: "110%",
+        rotateZ: "-70deg",
         easing: "easeInOutQuart",
     });
-    anime({
-        targets: firstname.querySelectorAll(".char p"),
-        translateX: 0,
-        easing: "easeInOutQuart",
-    });
+    anime(
+        {
+            targets: firstname.querySelectorAll(".char p"),
+            translateX: 0,
+            rotateZ: 0,
+            easing: "easeInOutQuart",
+        },
+        "-=1000"
+    );
 });
