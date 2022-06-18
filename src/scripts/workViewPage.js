b76dcc3b084d0b3432f452af15ba38f4f5b8f885
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-06-18 03:02:16 -0300

    Structured  and animated the top section of the projects

--- src/scripts/workViewPage.js
+++ src/scripts/workViewPage.js
@@ -4,14 +4,55 @@ import barba from "@barba/core";
 import { breakIntoWord } from "./index";
 
 function workViewPageInit() {
-    const t1 = anime.timeline();
+    const topSect = document.querySelector(".workViewPage #topSect");
+    const summary = topSect.querySelector("#summary");
+    const websiteBtn = topSect.querySelector("#website");
 
+    breakIntoWord(summary);
+
+    const t1 = anime.timeline();
     t1.add({
-        targets: backBtn.querySelector("p"),
+        targets: topSect.querySelector("h1 p"),
         translateY: ["120%", 0],
         easing: "easeOutQuad",
         duration: 500,
-    });
+    })
+        .add(
+            {
+                targets: summary.querySelectorAll(".word p"),
+                translateY: ["120%", 0],
+                easing: "easeOutQuad",
+                duration: 500,
+                delay: anime.stagger(10),
+            },
+            "-=300"
+        )
+        .add(
+            {
+                targets: websiteBtn,
+                clipPath: ["circle(0% at 50% 50%)", "circle(50% at 50% 50%)"],
+                easing: "easeInOutQuad",
+                duration: 700,
+                begin: () => {
+                    anime({
+                        targets: websiteBtn.querySelectorAll("p"),
+                        translateY: ["100%", 0],
+                        opacity: [0, 1],
+                        duration: 700,
+                        easing: "easeInOutQuad",
+                        delay: anime.stagger(100),
+                    });
+                },
+            },
+            "-=500"
+        )
+
+        .add({
+            targets: backBtn.querySelector("p"),
+            translateY: ["120%", 0],
+            easing: "easeOutQuad",
+            duration: 500,
+        });
 
     backBtn.addEventListener("click", () => {
         history.back();
